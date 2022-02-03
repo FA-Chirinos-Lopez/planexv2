aaee91e96d1a7ce37c367b1870d00c211ab7f185
FelixChirinosLopez <69871835+FA-Chirinos-Lopez@users.noreply.github.com>
2022-02-03 12:12:32 +0000

    a31

--- components/SeminarsFooter.js
+++ components/SeminarsFooter.js
@@ -26,16 +26,40 @@ export default function SeminarsFooter({EventName, EventStart, EventEnd, FooterI
         if(DateToCut){
             const cuttedDate = DateToCut.split("-")
             const t1 = cuttedDate[2]+"-"+cuttedDate[1]
-            return cuttedDate[2]
-          
+            
+            if(cuttedDate[2].slice(0,1)<=0)
+            {
+                return cuttedDate[2].slice(1,2)
+            }else{
+                return cuttedDate[2]
+            }
+            
+            
         }}
+
+    function getDayNumberN(DateToCut){
+        if(DateToCut){
+            const cuttedDate = DateToCut.split("-")
+            const t1 = cuttedDate[2]+"-"+cuttedDate[1]
+            
+            if(cuttedDate[2].slice(0,1)<=0)
+            {
+                return cuttedDate[2].slice(1,2)
+            }else{
+                return cuttedDate[2]
+            }
+            
+            
+        }}
+        
+
     const startDay=getDayNumber(EventStart)
     const startMonth=getMonth(EventStart)
     const endDay=getDayNumber(EventEnd)
     const endMonth=getMonth(EventEnd)
-    const toPrintInDate = "From "+startDay+" "+startMonth+" To "+endDay+" "+endMonth
+    const toPrintInDate = startDay+" "+startMonth+" - "+endDay+" "+endMonth
     if(startMonth==endMonth){
-        toPrintInDate=startDay+" - "+endDay+" "+endMonth
+        toPrintInDate=startDay+"-"+endDay+" "+endMonth
     }
 
     return (
@@ -45,7 +69,7 @@ export default function SeminarsFooter({EventName, EventStart, EventEnd, FooterI
             <h2 className="footerSeminars__grid__h2">{toPrintInDate}</h2>
         </div>
 
-        <div className="footerSeminars__flexIMG">
+        <div className="footerSeminars__flexIMG" >
             <img src={URL+FooterImage} alt="" className="footerSeminars__flexIMG__img"/>
         </div> 
     </footer>
